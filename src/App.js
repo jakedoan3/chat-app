9f68734677c2c57b47a078a554e3ba13dbeeff6c
Jake Doan <jakedoan3@gmail.com>
2023-08-15 12:31:24 -0500

    hello world

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { AuthContext } from "./context/AuthContext";
 import { Routes, Route, Navigate } from 'react-router-dom'
 
 
-// last updated August 14, 2023
+// last updated August 15, 2023
 // TO-DO:
   // Error handling:
     // Error message if no user is selected for chat
